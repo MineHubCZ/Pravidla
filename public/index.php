4e6a428be2f49502a6f6fd7d473fbef74e1d050b
TENMAJKL <tenmajkl@liskyjidlokod.fun>
2022-10-09 20:11:59 +0200

    adatps to new lemon

--- public/index.php
+++ public/index.php
@@ -1,16 +1,16 @@
 <?php
 
-use Lemon\Kernel\Lifecycle;
+use Lemon\Kernel\Application;
 use Lemon\Route;
 
 require __DIR__.'/../vendor/autoload.php';
 
-$lifecycle = Lifecycle::init(__DIR__);
+$app = Application::init(__DIR__);
 
-$lifecycle->get('templating');
+$app->get('templating');
 
 /** @var \Lemon\Templating\Enviroment $env */
-$env = $lifecycle->get('templating.env');
+$env = $app->get('templating.env');
 $env->macro('markdown', function(string $text) {
     $pd = new \Parsedown();
     $pd->setSafeMode(true);
@@ -24,16 +24,16 @@ $env->macro('markdown', function(string $text) {
     return $result;
 });
 
-Route::get('/', function(Lifecycle $lifecycle) {
+Route::get('/', function(Application $app) {
  
-    $metadata = file_get_contents($lifecycle->file('data.metadata', 'json'));
+    $metadata = file_get_contents($app->file('data.metadata', 'json'));
     $metadata = json_decode($metadata);
     
-    $last_edit = filemtime($lifecycle->file('data.compiled', 'md'));
+    $last_edit = filemtime($app->file('data.compiled', 'md'));
     $last_edit_formatted = date('d. m. Y H:i:s', $last_edit);
     $last_edit_year = date('Y', $last_edit);
 
-    $content = file_get_contents($lifecycle->file('data.compiled', 'md'));
+    $content = file_get_contents($app->file('data.compiled', 'md'));
 
     return template('home', ...compact('metadata', 'last_edit_year', 'last_edit_formatted', 'content'));
 });
@@ -41,5 +41,5 @@ Route::get('/', function(Lifecycle $lifecycle) {
 Route::template('about', 'about');
 
 Route::get('markdown', 
-    fn() => file_get_contents($lifecycle->file('data.compiled', 'md'))
+    fn() => file_get_contents($app->file('data.compiled', 'md'))
 );
